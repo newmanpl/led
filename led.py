f902839941c82f656b2412b870c67d09d5ee20e7
newmanpl <newmanpl@gmx.de>
2016-03-12 15:11:18 +0000

    use class structure

--- led.py
+++ led.py
@@ -2,147 +2,128 @@
 # encoding: utf-8
 
 ########################################################################
-# use pin 11, not GPIO11, pin 11 egal to GPIO17, pin16 egual to GPIO 23
+# use pin 11, not GPIO11, pin 11 egal to GPIO17, setting.getOutPin1()6 egual to GPIO 23
 # use pin 18, equal to GPIO 24
 #########################################################################
+
 import RPi.GPIO as GPIO
+from classes.setting import GPIOSetting,ProcessControl,LedControl
 import time
 import sys,os
 import thread
 import psutil
 import subprocess
 from os import getpid
-
-method="notify_led_state" 
-proc=subprocess.Popen(['sudo php -f /var/www/phptest/php_server.php '+method],shell=True,stdout=subprocess.PIPE); 
+'''
+method="notify_led_state"
+proc=subprocess.Popen(['sudo php -f /var/www/phptest/php_server.php '+method],shell=True,stdout=subprocess.PIPE);
 print("after popen");
 response=proc.stdout.read();
 print("after read");
 print("response %s", response);
-
+'''
 arg_len = len(sys.argv)
-led_on = False
-led_looping = False
-led_which = 1
-pin1 = 16
-pin2 = 18
-pin3 = 40
+g_led_on = False
+g_led_looping = False
+g_led_which = 1
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
+setting = GPIOSetting()
+setting.setMode(GPIO.BOARD)
+setting.setWarnings(False)
 
-GPIO.setup(pin1, GPIO.OUT)
-GPIO.setup(pin2, GPIO.OUT)
-GPIO.setup(pin3, GPIO.OUT)
+setting.setOutPin1(16, GPIO.OUT)
+setting.setOutPin2(18, GPIO.OUT)
+setting.setOutPin3(40, GPIO.OUT)
+
+ledCtrl = LedControl()
+
 print "enter python \r",
 for arg in sys.argv:
   if arg == 'looping':
-    led_looping = True
+    ledCtrl.setLed(0) #looping
   if arg == '1':
-    led_which = 1
+    ledCtrl.setLed(1)
   if arg == '2':
-    led_which = 2
+    ledCtrl.setLed(2)
   if arg == '3':
-    led_which = 3
+    ledCtrl.setLed(3)
 
-  print("which led %d\r", led_which),
+  print("which led %d\r", ledCtrl.getLed()),
 
   if arg == 'on':
-    led_on = True
+    ledCtrl.setState(True)
   else:
-    led_on = False
+    ledCtrl.setState(False)
 
 file_location = '/var/www/phptest/proc'
-file_existing = False
-try:
-  with open(file_location, 'r') as f:
-    running_proc = f.readline().rstrip()
-    file_existing = True
-    if led_looping and led_on and file_existing :
-      exit()
-except IOError:
-  print "[warning] first time the proc file is not existing"
 
-try:
-  if file_existing:
-    cmd = "sudo kill " + running_proc
-    print("cmd %s\n",cmd)
-    os.system(cmd)
-except:
-  print("[warning] proc is not existing")
+procCtrl = ProcessControl()
 
-mypid = getpid()
-print("current pid %d\n",mypid)
-target = open("/var/www/phptest/proc", 'a')
-target.truncate()
-target.write(str(mypid))
-target.close()
+pid = procCtrl.readPidFromFile(file_location)
+if pid != 0:
+   cmd = "sudo kill " + pid
+   print("cmd %s\n",cmd)
+   os.system(cmd)
 
-
-def wait_for_input(stop):
-    print("enter wait_for_input\n")
-    if led_on == False:
-      print("now callback stop looping\n")
-      stop.append(True)
-      led_reset()
-      GPIO.cleanup()
+procCtrl.writePidToFile(file_location, getpid())
 
 def led_reset():
 #    print("@@@ enter reset led\n")
-    led_looping = False
-    led_on = False
-    GPIO.output(pin1, GPIO.LOW)
-    GPIO.output(pin2, GPIO.LOW)
-    GPIO.output(pin3, GPIO.LOW)
+    ledCtrl.setState(True)
+    ledCtrl.setLed(0)
+    GPIO.output(setting.getOutPin1(), GPIO.LOW)
+    GPIO.output(setting.getOutPin2(), GPIO.LOW)
+    GPIO.output(setting.getOutPin3(), GPIO.LOW)
 
 def blink():
-    stop = []
-    print("call blink \r"),
-    while led_looping == True and led_on == True:
-      print("enter while led_looping %d led_on %d \r", led_looping, led_on),
+    while ledCtrl.getState() == True and ledCtrl.getLed() == 0:
       led_reset()
-      GPIO.output(pin1, GPIO.HIGH)
-      time.sleep(1)
+      GPIO.output(setting.getOutPin1(), GPIO.HIGH)
+      time.sleep(0.5)
       led_reset()
-      GPIO.output(pin2, GPIO.HIGH)
-      time.sleep(1)
+      GPIO.output(setting.getOutPin2(), GPIO.HIGH)
+      time.sleep(0.5)
       led_reset()
-      GPIO.output(pin3, GPIO.HIGH)
-      time.sleep(1)
-print("led_looping %d led on %d\n", led_looping, led_on),
+      GPIO.output(setting.getOutPin3(), GPIO.HIGH)
+      time.sleep(0.5)
+      print("######## blink: looping led %d state %d \n", ledCtrl.getLed(),ledCtrl.getState())
+#print("g_led_looping %d led on %d\n", g_led_looping, g_led_on),
 
 ###################################################
 # handle single light
 ####################################################
 #led_reset()
-if led_which == 1:
-  if led_on == True:
+print("######## led %d state %d \n", ledCtrl.getLed(),ledCtrl.getState())
+if ledCtrl.getLed() == 1:
+  if ledCtrl.getState() == True:
     print("@@@ light 1\n")
-    GPIO.output(pin1, GPIO.HIGH)
+    GPIO.output(setting.getOutPin1(), GPIO.HIGH)
   else:
     print("@@@ light 1 off")
-    GPIO.output(pin1, GPIO.LOW)
-if led_which == 2:
-  if led_on == True:
+    GPIO.output(setting.getOutPin1(), GPIO.LOW)
+
+if ledCtrl.getLed() == 2:
+  if ledCtrl.getState() == True:
     print("@@@ light 2\n")
-    GPIO.output(pin2, GPIO.HIGH)
+    GPIO.output(setting.getOutPin2(), GPIO.HIGH)
   else:
-    GPIO.output(pin2, GPIO.LOW)
+    GPIO.output(setting.getOutPin2(), GPIO.LOW)
 
-if led_which == 3:
-  if led_on == True:
+if ledCtrl.getLed() == 3:
+  if ledCtrl.getState() == True:
     print("@@@ light 3\n")
-    GPIO.output(pin3, GPIO.HIGH)
+    GPIO.output(setting.getOutPin3(), GPIO.HIGH)
   else:
-    GPIO.output(pin3, GPIO.LOW)
-
-if led_looping == True and led_on == True:
-  print("start looping\r"),
+    GPIO.output(setting.getOutPin3(), GPIO.LOW)
+print("####### please now check led %d state %d #####\r\n", ledCtrl.getLed(), ledCtrl.getState())
+if ledCtrl.getLed() == 0 and ledCtrl.getState() == True:
+  print("##### start looping\r\n")
 #  thread.start_new_thread(blink, ())
 #  time.sleep(2)
   blink()
-
-if led_looping == True and led_on == False:
+else:
+  print("### something is wrong ###\r\n")
+if ledCtrl.getLed() == 0 and ledCtrl.getState() == False:
   print("@@ stop looping\r"),
   led_reset()
   GPIO.cleanup()
@@ -150,4 +131,4 @@ cmd = 'sudo rm ' + file_location
 os.system(cmd)
 #call php back
 #thread.join()
-print("3 reach end of py endline led_looping %d\r", led_looping)
+#print("3 reach end of py endline g_led_looping %d\r", g_led_looping)
